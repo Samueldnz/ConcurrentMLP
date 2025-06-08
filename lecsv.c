1e50bdd63f3d8fc9af3db55bea25e72c0a34f0b1
Samueldnz <samuelsd@dcc.ufrj.br>
2025-06-07 21:12:02 -0300

    Fix bugs and MSE output

--- lecsv.c
+++ lecsv.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <time.h>  // ⏱️ para clock()
 
 #define ROWS 2000
 #define COLS 6
@@ -10,7 +10,7 @@
 #define OUTPUT_SIZE 1
 #define EPOCHS 1000
 #define LEARNING_RATE 0.1
-#define SAMPLES 2000 // quantidade de amostras
+#define SAMPLES 2000
 
 void read_csv_to_matrix(const char *filename, double matrix[ROWS][COLS]) {
     FILE *file = fopen(filename, "r");
@@ -26,7 +26,7 @@ void read_csv_to_matrix(const char *filename, double matrix[ROWS][COLS]) {
         char *ptr = line;
         for (int col = 0; col < COLS; col++) {
             matrix[row][col] = strtod(ptr, &ptr);
-            if (*ptr == ',') ptr++;  // Avança o ponteiro após a vírgula
+            if (*ptr == ',') ptr++;
         }
         row++;
     }
@@ -34,39 +34,40 @@ void read_csv_to_matrix(const char *filename, double matrix[ROWS][COLS]) {
     fclose(file);
 }
 
-float sigmoid(float x) {
-    return 1.0f / (1.0f + expf(-x));
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
 }
 
-float sigmoid_derivative(float x) {
-    float s = sigmoid(x);
+double sigmoid_derivative(double s) {
     return s * (1 - s);
 }
 
-void init_weights(float *w, int size, float min, float max) {
+void init_weights(double *w, int size, double min, double max) {
     for (int i = 0; i < size; i++) {
-        w[i] = ((float) rand() / RAND_MAX) * (max - min) + min;
+        w[i] = ((double) rand() / RAND_MAX) * (max - min) + min;
     }
 }
 
-void train(float inputs[SAMPLES][INPUT_SIZE], float targets[SAMPLES]) {
-    float w1[HIDDEN_SIZE][INPUT_SIZE];
-    float b1[HIDDEN_SIZE];
-    float w2[OUTPUT_SIZE][HIDDEN_SIZE];
-    float b2[OUTPUT_SIZE];
+void train(double inputs[SAMPLES][INPUT_SIZE], double targets[SAMPLES]) {
+    double w1[HIDDEN_SIZE][INPUT_SIZE];
+    double b1[HIDDEN_SIZE];
+    double w2[OUTPUT_SIZE][HIDDEN_SIZE];
+    double b2[OUTPUT_SIZE];
 
-    // Inicialização
-    init_weights(&w1[0][0], HIDDEN_SIZE * INPUT_SIZE, -1.0f, 1.0f);
-    init_weights(b1, HIDDEN_SIZE, -1.0f, 1.0f);
-    init_weights(&w2[0][0], OUTPUT_SIZE * HIDDEN_SIZE, -1.0f, 1.0f);
-    init_weights(b2, OUTPUT_SIZE, -1.0f, 1.0f);
+    init_weights(&w1[0][0], HIDDEN_SIZE * INPUT_SIZE, -1.0, 1.0);
+    init_weights(b1, HIDDEN_SIZE, -1.0, 1.0);
+    init_weights(&w2[0][0], OUTPUT_SIZE * HIDDEN_SIZE, -1.0, 1.0);
+    init_weights(b2, OUTPUT_SIZE, -1.0, 1.0);
+
+    // ⏱️ Início da contagem de tempo
+    clock_t start_time = clock();
 
     for (int epoch = 0; epoch < EPOCHS; epoch++) {
-        float total_error = 0;
+        double total_error = 0.0;
 
         for (int s = 0; s < SAMPLES; s++) {
-            float hidden[HIDDEN_SIZE];
-            float output[OUTPUT_SIZE];
+            double hidden[HIDDEN_SIZE];
+            double output[OUTPUT_SIZE];
 
             // FORWARD
             for (int i = 0; i < HIDDEN_SIZE; i++) {
@@ -85,13 +86,13 @@ void train(float inputs[SAMPLES][INPUT_SIZE], float targets[SAMPLES]) {
                 output[i] = sigmoid(output[i]);
             }
 
-            float error = targets[s] - output[0];
+            double error = targets[s] - output[0];
             total_error += error * error;
 
             // BACKPROPAGATION
-            float d_output = error * sigmoid_derivative(output[0]);
+            double d_output = error * sigmoid_derivative(output[0]);
 
-            float d_hidden[HIDDEN_SIZE];
+            double d_hidden[HIDDEN_SIZE];
             for (int i = 0; i < HIDDEN_SIZE; i++) {
                 d_hidden[i] = d_output * w2[0][i] * sigmoid_derivative(hidden[i]);
             }
@@ -111,10 +112,13 @@ void train(float inputs[SAMPLES][INPUT_SIZE], float targets[SAMPLES]) {
             }
         }
 
-        if (epoch % 100 == 0) {
-            printf("Época %d - Erro médio quadrático: %.4f\n", epoch, total_error / SAMPLES);
-        }
+        printf("Época %d - Erro médio quadrático: %.6f\n", epoch, total_error / SAMPLES);
     }
+
+    // ⏱️ Fim da contagem e exibição do tempo total
+    clock_t end_time = clock();
+    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("\n⏱️ Tempo total de treinamento: %.2f segundos\n", time_taken);
 }
 
 int main(int argc, char *argv[]) {
@@ -123,37 +127,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    srand(time(NULL));
+
     double data[ROWS][COLS];
-    float inputs[SAMPLES][INPUT_SIZE];
-    float targets[SAMPLES];
+    double inputs[SAMPLES][INPUT_SIZE];
+    double targets[SAMPLES];
 
     read_csv_to_matrix(argv[1], data);
 
-    // Transfere os dados para a rede
     for (int i = 0; i < SAMPLES; i++) {
         for (int j = 0; j < INPUT_SIZE; j++) {
-            inputs[i][j] = (float)data[i][j]; // entrada
+            inputs[i][j] = data[i][j];
         }
-        targets[i] = (float)data[i][INPUT_SIZE]; // última coluna como saída
+        targets[i] = data[i][INPUT_SIZE];
     }
 
     train(inputs, targets);
 
-    return 0;int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: %s <arquivo.csv>\n", argv[0]);
-        return 1;
-    }
-
-    double data[ROWS][COLS];
-    read_csv_to_matrix(argv[1], data);
-
-    float inputs[SAMPLES][INPUT_SIZE];
-    float targets[SAMPLES];
-
-    train(inputs, targets);
-    
     return 0;
-}
-
 }
